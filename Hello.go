fbbf2e1ec1cc842e64437ef7388b249cb9f5b8a0
Sha1189 <shaellaghapar@gmail.com>
2022-03-28 12:16:52 +0800

    added hello world string

--- Hello.go
+++ Hello.go
@@ -1,12 +1,15 @@
 package main
 
-func HelloWorld() string {
-	//Insert your code here
+import "fmt"
 
+func HelloWorld() string {
+
+	messageHelloWorld := "Hello World from GO! " //Insert your code here
 	//Do not remove this line here
 	return messageHelloWorld
 }
 
 func main() {
 	HelloWorld()
+	fmt.Println("Hello World from GO! ")
 }
